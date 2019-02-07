55b7045b9a9c26e0562fb222121d28dd3a975fb6
tkaneda0078 <1111510078r21@gmail.com>
2019-02-07 10:05:05 +0900

    api endpoint作成

---
+++ fuel/app/classes/controller/user.php
@@ -0,0 +1,59 @@
+<?php
+
+use \Fuel\Core\Controller_Rest;
+
+class Controller_User extends Controller_Rest
+{
+  /**
+   * response format
+   *
+   * @var string
+   */
+  protected $format = 'json';
+
+  /**
+   * get user data
+   *
+   */
+  public function get_data()
+  {
+    $user_id = ($this->param('id') !== '') ? $this->param('id') : '';
+
+    $users = [];
+    if ($user_id) {
+      // todo: get user data
+    } else {
+      // todo: get user list
+    }
+
+    return $this->response($users);
+  }
+
+  /**
+   * create user
+   *
+   */
+  public function post_data()
+  {
+    // todo: create user
+  }
+
+  /**
+   * update user data
+   *
+   */
+  public function put_data()
+  {
+    // todo: user data update
+  }
+
+  /**
+   * delete user
+   *
+   */
+  public function delete_data()
+  {
+    // todo: delete user
+  }
+
+}
--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,7 +1,16 @@
 <?php
+
+use \Fuel\Core\Route;
+
 return array(
-	'_root_'  => 'welcome/index',  // The default route
-	'_404_'   => 'welcome/404',    // The main 404 route
-	
-	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
+  // user
+  'api/v1/users' => [
+    ['GET', new Route('user/data')],
+    ['POST', new Route('user/data')]
+  ],
+  'api/v1/users/:id' => [
+    ['GET', new Route('user/data/$1')],
+    ['PUT', new Route('user/data/$1')],
+    ['DELETE', new Route('user/data/$1')]
+  ]
 );
